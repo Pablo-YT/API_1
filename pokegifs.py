7ada01e31f99c1e79b517ca885e4d72c9562c3e3
Josh_Date <jjdate@live.com>
2019-03-21 16:07:01 -0400

    d

--- pokegifs.py
+++ pokegifs.py
@@ -5,4 +5,5 @@ key = os.environ.get("GIPHY_KEY")
 
 
 url = "https://api.giphy.com/v1/gifs/search?api_key=2ZvDWyUWRleLV9905xUu9TFJolpRULZN&q=videogames&rating=g".format(key)
-print(requests.get(url))
+requests.get(url)
+print(url)
